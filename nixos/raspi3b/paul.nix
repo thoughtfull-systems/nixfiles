cbc0cbf1fecc28148ad09dd40696bb8c9575356f
Paul Stadig <paul@thoughtfull.systems>
2023-07-19 17:56:03 -0400

    Change paul's password on raspi3b

--- nixos/raspi3b/paul.nix
+++ nixos/raspi3b/paul.nix
@@ -3,7 +3,7 @@
   users.users.paul = {
     extraGroups = [ "wheel" ];
     group = "users";
-    hashedPassword = "$6$iYEfcmTMT5gQiFJs$FNXGi9z7iO1M.XqsTKYvlB6uszO9liPFs2cNSFOEe4Rf3sNy0LeEaoUwaC0EXbt/Q.ZKRa/WGobOSnOz.KPQS/";
+    hashedPassword = "$6$FHwQvB6VtmKnyx3V$BNZLfh7N3Ja.YnVh8fPKrRsKmEM.APdUtJwp1/xhQFqJ/1FxgzRQE.V4qMOPOfMqCOV8T9GkkSRM3urafaSK70";
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAB2hfargabOYq6TZ+U6zXUZG+SWrxWdV0Fq5AbhDLghAL4kdwi1j5Q9C8ki622ZwIkk+v7+575IXgyezlHIHjIFFwDf09ODfTPVSwNizpRBK8uMX1YV0XpULJmV8nOJFF0gbn9gQNktM6Obfuhl7QBGhmpEvnvROsBaAqU8OqcQeMRg+w== paul@carbon"
