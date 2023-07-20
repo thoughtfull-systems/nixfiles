5ebb737244630c57708e3f9ea955c1e9962fb14d
Paul Stadig <paul@thoughtfull.systems>
2023-07-20 10:24:00 -0400

    Add autoStart option to VPNs

--- nixosModules/thoughtfull/vpn-home.nix
+++ nixosModules/thoughtfull/vpn-home.nix
@@ -2,15 +2,22 @@
   cfg = config.thoughtfull.vpn.home;
   secrets = config.age.secrets;
 in {
-  options.thoughtfull.vpn.home.enable = lib.mkEnableOption "proton vpn";
+  options.thoughtfull.vpn.home = {
+    enable = lib.mkEnableOption "Home VPN";
+    autoStart = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = lib.mdDoc "Whether Home VPN should be started automatically.";
+    };
+  };
   config = lib.mkIf cfg.enable {
     age.secrets = {
-      vpn-home-config.file = ../../age/secrets/vpn-home-config.age;
       vpn-home-askpass.file = ../../age/secrets/vpn-home-askpass.age;
+      vpn-home-config.file = ../../age/secrets/vpn-home-config.age;
     };
     services = lib.mkDefault {
       openvpn.servers.home = {
-        autoStart = false;
+        autoStart = lib.mkDefault cfg.autoStart;
         config = ''
           config ${secrets.vpn-home-config.path}
           askpass ${secrets.vpn-home-askpass.path}
--- nixosModules/thoughtfull/vpn-proton.nix
+++ nixosModules/thoughtfull/vpn-proton.nix
@@ -2,14 +2,22 @@
   cfg = config.thoughtfull.vpn.proton;
   secrets = config.age.secrets;
 in {
-  options.thoughtfull.vpn.proton.enable = lib.mkEnableOption "proton vpn";
+  options.thoughtfull.vpn.proton = {
+    enable = lib.mkEnableOption "Proton VPN";
+    autoStart = lib.mkOption {
+      default = true;
+      type = lib.types.bool;
+      description = lib.mdDoc "Whether Proton VPN should be started automatically.";
+    };
+  };
   config = lib.mkIf cfg.enable {
     age.secrets = {
-      vpn-proton-config.file = ../../age/secrets/vpn-proton-config.age;
       vpn-proton-auth-user-pass.file = ../../age/secrets/vpn-proton-auth-user-pass.age;
+      vpn-proton-config.file = ../../age/secrets/vpn-proton-config.age;
     };
     services = lib.mkDefault {
       openvpn.servers.proton = {
+        autoStart = lib.mkDefault cfg.autoStart;
         config = ''
           config ${secrets.vpn-proton-config.path}
           auth-user-pass ${secrets.vpn-proton-auth-user-pass.path}
