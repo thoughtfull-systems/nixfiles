b621e76ff47523a18e1f325d31c4f467426de217
Paul Stadig <paul@thoughtfull.systems>
2023-07-23 19:03:27 -0400

    Use new xfce.excludePackages option

--- nixosModules/thoughtfull/xfce.nix
+++ nixosModules/thoughtfull/xfce.nix
@@ -22,12 +22,15 @@ lib.mkIf config.services.xserver.desktopManager.xfce.enable {
       };
     })
   ];
-  environment.systemPackages = with pkgs.xfce; [
-    xfce4-panel
-    xfce4-pulseaudio-plugin
-    xfce4-xkb-plugin
-    xfce4-weather-plugin
-  ];
+  environment = {
+    systemPackages = with pkgs.xfce; [
+      xfce4-panel
+      xfce4-pulseaudio-plugin
+      xfce4-xkb-plugin
+      xfce4-weather-plugin
+    ];
+    xfce.excludePackages = [ pkgs.xfce.xfce4-volumed-pulse ];
+  };
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
     thunar-media-tags-plugin
@@ -39,7 +42,6 @@ lib.mkIf config.services.xserver.desktopManager.xfce.enable {
         noDesktop = lib.mkDefault true;
         enableScreensaver = lib.mkDefault false;
         enableXfwm = lib.mkDefault false;
-        excludePackages = [ pkgs.xfce.xfce4-volumed-pulse ];
       };
       displayManager.lightdm.enable = true;
     };
