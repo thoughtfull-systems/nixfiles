de1b00f950459bf724d54322161548ef79bb1d40
Paul Stadig <paul@thoughtfull.systems>
2023-08-22 07:17:40 -0400

    Remove paul nixos module

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,7 +1,5 @@
 inputs: rec {
   default = thoughtfull;
   home = inputs.self.homeManagerModules;
-  paul = import ./paul.nix;
   thoughtfull = import ./thoughtfull inputs;
-  root = import ./root.nix;
 }
--- nixosModules/paul/authorizedKeys/default.nix
+++
@@ -1,4 +0,0 @@
-[
-  (builtins.readFile ./carbon.pub)
-  (builtins.readFile ./hemera.stadig.name.pub)
-]
