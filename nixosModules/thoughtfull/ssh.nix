5d6b311d8110fe7491245845d614e7a3fb66493c
Paul Stadig <paul@thoughtfull.systems>
2023-10-21 15:04:47 -0400

    Configure unlock.* wildcard for SSH

--- nixosModules/thoughtfull/ssh.nix
+++ nixosModules/thoughtfull/ssh.nix
@@ -1,16 +1,24 @@
 { ... } : {
-  programs.ssh.knownHosts = {
-    "github-ecdsa" = {
-      hostNames = [ "github.com" ];
-      publicKey = builtins.readFile ./ssh/github-ecdsa.pub;
-    };
-    "github-ed25519" = {
-      hostNames = [ "github.com" ];
-      publicKey = builtins.readFile ./ssh/github-ed25519.pub;
-    };
-    "github-rsa" = {
-      hostNames = [ "github.com" ];
-      publicKey = builtins.readFile ./ssh/github-rsa.pub;
+  programs.ssh = {
+    extraConfig = ''
+      Host unlock.*
+      RemoteCommand cryptsetup-askpass
+      RequestTTY yes
+      User root
+    '';
+    knownHosts = {
+      "github-ecdsa" = {
+        hostNames = [ "github.com" ];
+        publicKey = builtins.readFile ./ssh/github-ecdsa.pub;
+      };
+      "github-ed25519" = {
+        hostNames = [ "github.com" ];
+        publicKey = builtins.readFile ./ssh/github-ed25519.pub;
+      };
+      "github-rsa" = {
+        hostNames = [ "github.com" ];
+        publicKey = builtins.readFile ./ssh/github-rsa.pub;
+      };
     };
   };
 }
