4a924af6ce40a5ae36080ef3bf79d4f6ef266db5
Paul Stadig <paul@thoughtfull.systems>
2023-12-09 06:40:12 -0500

    Change postgres backup to run at *:55

--- nixosModules/thoughtfull/postgresql-backup.nix
+++ nixosModules/thoughtfull/postgresql-backup.nix
@@ -2,7 +2,7 @@
   config = lib.mkIf config.services.postgresql.enable {
     services.postgresqlBackup = {
       enable = true;
-      startAt = lib.mkDefault "*-*-* *:00:00";
+      startAt = lib.mkDefault "*-*-* *:55:00";
     };
   };
 }
