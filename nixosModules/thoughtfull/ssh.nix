81a9bfe225fc49ed8d9315d821bcb39e286e629d
Paul Stadig <paul@thoughtfull.systems>
2023-12-13 05:08:38 -0500

    This doesn't work because I cannot set 'Hostname"

--- nixosModules/thoughtfull/ssh.nix
+++ nixosModules/thoughtfull/ssh.nix
@@ -1,24 +1,16 @@
 { ... } : {
-  programs.ssh = {
-    extraConfig = ''
-      Host unlock.*
-      RemoteCommand cryptsetup-askpass
-      RequestTTY yes
-      User root
-    '';
-    knownHosts = {
-      "github-ecdsa" = {
-        hostNames = [ "github.com" ];
-        publicKey = builtins.readFile ./ssh/github-ecdsa.pub;
-      };
-      "github-ed25519" = {
-        hostNames = [ "github.com" ];
-        publicKey = builtins.readFile ./ssh/github-ed25519.pub;
-      };
-      "github-rsa" = {
-        hostNames = [ "github.com" ];
-        publicKey = builtins.readFile ./ssh/github-rsa.pub;
-      };
+  programs.ssh.knownHosts = {
+    "github-ecdsa" = {
+      hostNames = [ "github.com" ];
+      publicKey = builtins.readFile ./ssh/github-ecdsa.pub;
+    };
+    "github-ed25519" = {
+      hostNames = [ "github.com" ];
+      publicKey = builtins.readFile ./ssh/github-ed25519.pub;
+    };
+    "github-rsa" = {
+      hostNames = [ "github.com" ];
+      publicKey = builtins.readFile ./ssh/github-rsa.pub;
     };
   };
 }
