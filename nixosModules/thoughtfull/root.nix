e5503c4fdef9a6f53082657647b1dfae410205b3
Paul Stadig <paul@thoughtfull.systems>
2023-12-15 05:47:55 -0500

    Don't provide a default home manager stateVersion for root

--- nixosModules/thoughtfull/root.nix
+++ nixosModules/thoughtfull/root.nix
@@ -1,21 +1,11 @@
 { lib, pkgs, ... } : {
   home-manager.users.root = {
-    home = {
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      stateVersion = lib.mkDefault "22.11";
-    };
+    programs.tmux.enable = lib.mkDefault true;
     thoughtfull.desktop.enable = lib.mkOverride 900 false;
   };
   services.openssh.settings.PermitRootLogin = lib.mkDefault "prohibit-password";
   users.users.root = lib.mkDefault {
-    password = null;
-    shell = pkgs.zsh;
+    password = lib.mkDefault null;
+    shell = lib.mkDefault pkgs.zsh;
   };
 }
