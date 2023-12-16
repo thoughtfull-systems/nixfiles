f7bbf24650391c49b4afdd64416dc59b3ab6470e
Paul Stadig <paul@thoughtfull.systems>
2023-12-16 05:12:33 -0500

    Enable zsh configuration for root

--- nixosModules/thoughtfull/root.nix
+++ nixosModules/thoughtfull/root.nix
@@ -1,6 +1,9 @@
 { lib, pkgs, ... } : {
   home-manager.users.root = {
-    programs.tmux.enable = lib.mkOverride 900 true;
+    programs = {
+      tmux.enable = lib.mkOverride 900 true;
+      zsh.enable = lib.mkDefault true;
+    };
     thoughtfull.desktop.enable = lib.mkOverride 900 false;
   };
   services.openssh.settings.PermitRootLogin = lib.mkDefault "prohibit-password";
