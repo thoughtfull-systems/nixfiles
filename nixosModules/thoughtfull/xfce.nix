c1fca30b895bbddf55cbe1edd5248b079f64354b
Paul Stadig <paul@thoughtfull.systems>
2024-03-27 13:25:39 -0400

    I use suspend instead of hybrid sleep
    These overlays are not necessary anymore, because I use suspend instead of hybrid sleep.
    
    I can reconsider in the future if upstream implements the feature.

--- nixosModules/thoughtfull/xfce.nix
+++ nixosModules/thoughtfull/xfce.nix
@@ -1,38 +1,5 @@
 { config, lib, pkgs, utils, ... }:
 lib.mkIf config.services.xserver.desktopManager.xfce.enable {
-  nixpkgs.overlays = [
-    (self: super: {
-      xfce = super.xfce // {
-        libxfce4ui = super.xfce.libxfce4ui.overrideAttrs (
-          prevAttrs: {
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.xfce.org";
-              owner = "xfce";
-              repo = "libxfce4ui";
-              rev = "libxfce4ui-4.18.4";
-              sha256 = "sha256-HnLmZftvFvQAvmQ7jZCaYAQ5GB0YMjzhqZkILzvifoE=";
-            };
-            version = "4.18.4";
-          }
-        );
-        xfce4-power-manager = super.xfce.xfce4-power-manager.overrideAttrs (
-          prevAttrs: {
-            buildInputs = (utils.removePackagesByName
-              prevAttrs.buildInputs
-              [ super.xfce.libxfce4ui ]) ++
-            [ self.xfce.libxfce4ui ];
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.xfce.org";
-              owner = "pjstadig";
-              repo = "xfce4-power-manager";
-              rev = "xfce-4.18";
-              sha256 = "sha256-nWvyt9nMYU7erHftmEzIGOiNpELlHyyJvg17pA29Rsc=";
-            };
-            version = "4.18.2";
-          });
-      };
-    })
-  ];
   environment = {
     systemPackages = with pkgs.xfce; [
       xfce4-panel
