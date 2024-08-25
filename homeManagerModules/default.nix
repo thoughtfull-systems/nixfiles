3626e6a471094ea7812cf1e1fd06210afe012e83
Technosophist <technosophist@thoughtfull.systems>
2024-08-25 17:11:46 -0400

    Refactor home manager modules

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,3 +1,26 @@
 {
-  default = import ./thoughtfull;
+  default = { lib, ... }: {
+    imports = [
+      ./clojure.nix
+      ./desktop
+      ./emacs.nix
+      ./emacs/gtd.nix
+      ./emoji.nix
+      ./exwm.nix
+      ./git.nix
+      ./gnome-terminal.nix
+      ./home-manager.nix
+      ./javascript.nix
+      ./keychain.nix
+      ./notifications.nix
+      ./overlay-emacs.nix
+      ./autostart.nix
+      ./starship.nix
+      ./syncthing.nix
+      ./tmux.nix
+      ./yubikey.nix
+      ./zsh.nix
+    ];
+    systemd.user.startServices = lib.mkDefault "sd-switch";
+  };
 }
