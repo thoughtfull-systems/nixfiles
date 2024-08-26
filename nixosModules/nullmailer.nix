aa669883c2f88c73033fa1bb1e95d9f46630fe50
Technosophist <technosophist@thoughtfull.systems>
2024-08-26 06:28:42 -0400

    Pass in nullmailer secrets instead of pull them from environment

--- nixosModules/nullmailer.nix
+++ nixosModules/nullmailer.nix
@@ -1,5 +1,19 @@
-{ config, lib, ... } : {
-  services.nullmailer = {
-    remotesFile = config.age.secrets.nullmailer-remotes.path;
+{ config, lib, ... } : let
+  cfg = config.thoughtfull.nullmailer;
+in {
+  options.thoughtfull.nullmailer.age = {
+    remotesFile = lib.mkOption {
+      description = "Encrypted age file containing nullmailer remotes";
+      type = lib.types.path;
+    };
+  };
+  config = lib.mkIf config.services.nullmailer.enable {
+    age.secrets.thoughtfull-nullmailer-remotes = {
+      file = cfg.age.remotesFile;
+      owner = config.services.nullmailer.user;
+    };
+    services.nullmailer = {
+      remotesFile = config.age.secrets.thoughtfull-nullmailer-remotes.path;
+    };
   };
 }
