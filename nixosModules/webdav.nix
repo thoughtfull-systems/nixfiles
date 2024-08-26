8c48dd2cd633d20f0c14ff400c389d57a5948d4b
Technosophist <technosophist@thoughtfull.systems>
2024-08-26 15:07:17 -0400

    Take age encrypted file as option for webdav

--- nixosModules/webdav.nix
+++ nixosModules/webdav.nix
@@ -1,28 +1,46 @@
 { config, lib, ... }: let
-  cfg = config.services.webdav;
+  cfg = config.thoughtfull.webdav;
+  webdav = config.services.webdav;
 in {
-  services.webdav.settings = {
-    address = lib.mkDefault "127.0.0.1";
-    auth = lib.mkDefault true;
-    cors = {
-      enabled = lib.mkDefault false;
-      credentials = lib.mkDefault false;
+  options.thoughtfull.webdav.age = {
+    environmentFile = lib.mkOption {
+      type = lib.types.path;
+      default = null;
+      description = ''
+        Age encrypted environment file as defined in {manpage}`systemd.exec(5)`
+      '';
     };
-    modify = lib.mkDefault true;
-    port = lib.mkDefault 8001;
-    scope = lib.mkDefault "/var/lib/webdav";
-    tls = lib.mkDefault false;
-    users = [{
-      username = "webdav";
-      password = "{env}WEBDAV_PASSWORD";
-    }];
   };
-  systemd.services.webdav.serviceConfig = lib.mkIf cfg.enable {
-    StateDirectory = lib.mkDefault "webdav";
-    StateDirectoryMode = lib.mkDefault "0700";
-  };
-  thoughtfull = {
-    restic.paths = lib.mkIf cfg.enable [ cfg.settings.scope ];
-    systemd-notify-failure.services = lib.mkIf cfg.enable [ "webdav" ];
+  config = {
+    age.secrets = lib.mkIf webdav.enable {
+      thoughtfull-webdav-env-file.file = cfg.age.environmentFile;
+    };
+    services.webdav = {
+      environmentFile = config.age.secrets.thoughtfull-webdav-env-file.path;
+      settings = {
+        address = lib.mkDefault "127.0.0.1";
+        auth = lib.mkDefault true;
+        cors = {
+          enabled = lib.mkDefault false;
+          credentials = lib.mkDefault false;
+        };
+        modify = lib.mkDefault true;
+        port = lib.mkDefault 8001;
+        scope = lib.mkDefault "/var/lib/webdav";
+        tls = lib.mkDefault false;
+        users = [{
+          username = "webdav";
+          password = "{env}WEBDAV_PASSWORD";
+        }];
+      };
+    };
+    systemd.services.webdav.serviceConfig = lib.mkIf webdav.enable {
+      StateDirectory = lib.mkDefault "webdav";
+      StateDirectoryMode = lib.mkDefault "0700";
+    };
+    thoughtfull = {
+      restic.paths = lib.mkIf webdav.enable [ webdav.settings.scope ];
+      systemd-notify-failure.services = lib.mkIf webdav.enable [ "webdav" ];
+    };
   };
 }
