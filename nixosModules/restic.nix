3db809049812048af84810684540552346aa4749
Technosophist <technosophist@thoughtfull.systems>
2024-08-26 14:47:04 -0400

    Take age encrypted files as option to restic module

--- nixosModules/restic.nix
+++ nixosModules/restic.nix
@@ -2,18 +2,27 @@
   cfg = config.thoughtfull.restic;
 in {
   options.thoughtfull.restic = {
+    age = {
+      environmentFile = lib.mkOption {
+        type = lib.types.path;
+        default = null;
+        description = lib.mdDoc ''
+          Age encrypted file containing the credentials to access the repository, in the format of
+          an EnvironmentFile as described by systemd.exec(5)
+        '';
+      };
+      passwordFile = lib.mkOption {
+        type = lib.types.path;
+        default = null;
+        description = lib.mdDoc ''
+          Age encrypted file containing the repository password
+        '';
+      };
+    };
     s3Bucket = lib.mkOption {
       type = lib.types.str;
       default = null;
       description = "Name of S3 bucket to store backups";
-    };
-    environmentFile = lib.mkOption {
-      type = lib.types.str;
-      default = null;
-      description = lib.mdDoc ''
-        file containing the credentials to access the repository, in the format of an
-        EnvironmentFile as described by systemd.exec(5)
-      '';
     };
     exclude = lib.mkOption {
       type = lib.types.listOf lib.types.str;
@@ -28,13 +37,6 @@ in {
         "/home/*/.cache"
         ".git"
       ];
-    };
-    passwordFile = lib.mkOption {
-      type = lib.types.str;
-      default = null;
-      description = lib.mdDoc ''
-        Read the repository password from a file.
-      '';
     };
     paths = lib.mkOption {
       type = lib.types.listOf lib.types.str;
@@ -51,20 +53,27 @@ in {
       ];
     };
   };
-  config = lib.mkIf (cfg.paths != []) {
+  config = lib.mkIf (cfg.paths != []) (let
+    env-path = config.age.secrets.thoughtfull-restic-env-file.path;
+    pwd-path = config.age.secrets.thoughtfull-restic-pwd-file.path;
+  in {
+    age.secrets = {
+      thoughtfull-restic-env-file.file = cfg.age.environmentFile;
+      thoughtfull-restic-pwd-file.file = cfg.age.passwordFile;
+    };
     environment.systemPackages = [
       (pkgs.writeScriptBin "restic" ''
         #!${pkgs.bash}/bin/bash
-        ${pkgs.execline}/bin/envfile ${cfg.environmentFile} ${pkgs.restic}/bin/restic \
+        ${pkgs.execline}/bin/envfile ${env-path} ${pkgs.restic}/bin/restic \
           -r "s3:s3.amazonaws.com/${cfg.s3Bucket}" \
-          -p ${cfg.passwordFile} \
+          -p ${pwd-path} \
           "''${@}"
       '')
     ];
     services.restic.backups.default = {
-      environmentFile = cfg.environmentFile;
+      environmentFile = env-path;
       exclude = cfg.exclude;
-      passwordFile = cfg.passwordFile;
+      passwordFile = pwd-path;
       paths = cfg.paths;
       pruneOpts = [
         "--keep-daily 7"
@@ -76,5 +85,5 @@ in {
       timerConfig.OnCalendar = lib.mkDefault "*-*-* *:00:00";
     };
     thoughtfull.systemd-notify-failure.services = [ "restic-backups-default" ];
-  };
+  });
 }
