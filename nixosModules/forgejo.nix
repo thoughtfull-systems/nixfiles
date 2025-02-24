1c53da8775f7e61bc9e91987ab5d0993a7241ba6
technosophist <technosophist@thoughtfull.systems>
2025-02-24 06:07:20 -0500

    Use latest forgejo

--- nixosModules/forgejo.nix
+++ nixosModules/forgejo.nix
@@ -1,14 +1,16 @@
-{ config, lib, ... }: let
+{ config, lib, pkgs, ... }: let
   cfg = config.services.forgejo;
 in {
   services.forgejo = {
     database.type = lib.mkDefault "postgres";
     dump.enable = lib.mkDefault true;
+    package = pkgs.forgejo;
     settings = {
       service.DISABLE_REGISTRATION = lib.mkDefault true;
       server.HTTP_PORT = lib.mkDefault 8003;
     };
   };
+  environment.systemPackages = lib.mkIf cfg.enable [ pkgs.forgejo ];
   thoughtfull = lib.mkIf cfg.enable {
     restic.paths = [ cfg.dump.backupDir ];
     systemd-notify-failure.services = [ "forgejo" ];
