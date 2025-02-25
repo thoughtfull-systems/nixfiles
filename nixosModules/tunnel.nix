3f0e80aebeee398d8e4804f66efb580e45703afa
technosophist <technosophist@thoughtfull.systems>
2025-02-25 07:51:00 -0500

    Add many more features to tunnel
    - configures multiple tunnels
    - takes address and ports

--- nixosModules/tunnel.nix
+++ nixosModules/tunnel.nix
@@ -1,44 +1,93 @@
 { config, lib, pkgs, ... } : let
-  cfg = config.thoughtfull.tunnel;
-  portStrs = builtins.map builtins.toString cfg.ports;
-  portToArg = portStr: " -R${portStr}:localhost:${portStr}";
-  portsString = lib.strings.concatMapStrings portToArg portStrs;
+  cfg = config.thoughtfull.tunnels;
 in {
-  options.thoughtfull.tunnel = {
-    enable = lib.mkEnableOption "tunnel";
-    user = lib.mkOption {
-      default = "root";
-      description = "user to use connecting to host";
-      type = lib.types.str;
-    };
-    host = lib.mkOption {
-      description = "host to tunnel to";
-      type = lib.types.str;
-    };
-    identity = lib.mkOption {
-      description = "path to identity file to use for tunnel";
-      type = lib.types.str;
-    };
-    ports = lib.mkOption {
-      description = "ports to tunnel";
-      type = lib.types.listOf lib.types.int;
-    };
-  };
-  config = lib.mkIf (cfg.enable && (builtins.length cfg.ports) > 0) {
-    systemd.services.thoughtfull-tunnel = {
-      after = [ "network.target" ];
-      enable = true;
-      script = ''
-        ${pkgs.openssh}/bin/ssh -N -o ExitOnForwardFailure=yes -i ${cfg.identity} ${cfg.user}@${cfg.host} ${portsString}
-      '';
-      serviceConfig = {
-        Restart = "always";
-        Type = "exec";
+  options.thoughtfull.tunnels = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
+      options = {
+        name = lib.mkOption {
+          type = lib.types.str;
+          default = name;
+          description = "name of the tunnel";
+        };
+        enable = lib.mkOption {
+          default = true;
+          description = "enable the tunnel";
+          type = lib.types.bool;
+        };
+        user = lib.mkOption {
+          default = "root";
+          description = "user to connect with";
+          type = lib.types.str;
+        };
+        host = lib.mkOption {
+          description = "host to tunnel to";
+          type = lib.types.str;
+        };
+        identity = lib.mkOption {
+          description = "path to identity file to use for connection";
+          type = lib.types.str;
+        };
+        bindings = lib.mkOption {
+          description = "addresses and ports to bind";
+          type = lib.types.listOf (lib.types.submodule {
+            options = {
+              local = {
+                address = lib.mkOption {
+                  default = "localhost";
+                  description = "address to bind on the local end";
+                  type = lib.types.str;
+                };
+                port = lib.mkOption {
+                  description = "port to bind on the local end";
+                  type = lib.types.int;
+                };
+              };
+              remote = {
+                address = lib.mkOption {
+                  default = "localhost";
+                  description = "address to bind on the remote end";
+                  type = lib.types.str;
+                };
+                port = lib.mkOption {
+                  description = "port to bind on the remote end";
+                  type = lib.types.int;
+                };
+              };
+            };
+          });
+        };
       };
-      wantedBy = [ "multi-user.target" ];
-    };
-    thoughtfull = {
-      systemd-notify-failure.services = [ "thoughtfull-tunnel" ];
-    };
+    }));
+  };
+  config = lib.mkIf (builtins.length (builtins.attrNames cfg) > 0) {
+    systemd.services = builtins.foldl'
+      (services: name: let
+        c = cfg.${name};
+        endpoint = { address, port }: "${address}:${builtins.toString port}";
+        bindings = (lib.strings.concatMapStringsSep " "
+          (binding: "-R${endpoint binding.remote}:${endpoint binding.local}")
+          c.bindings);
+      in services // {
+        "thoughtfull-tunnel-${name}" = {
+          after = [ "network.target" ];
+          enable = c.enable;
+          script = ''
+            ${pkgs.openssh}/bin/ssh -N -o ExitOnForwardFailure=yes \
+                -i ${c.identity} \
+                ${bindings} \
+                ${c.user}@${c.host}
+          '';
+          serviceConfig = {
+            Restart = "always";
+            Type = "exec";
+          };
+          wantedBy = [ "multi-user.target" ];
+        };
+      })
+      {}
+      (builtins.attrNames cfg);
+    thoughtfull.systemd-notify-failure.services = (builtins.map
+      (name: "thoughtfull-tunnel-${name}")
+      (builtins.attrNames cfg));
   };
 }
