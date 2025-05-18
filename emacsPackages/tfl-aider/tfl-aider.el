24182785490b33e419190ded8a37d26138a56291
technosophist <technosophist@thoughtfull.systems>
2025-05-18 07:04:29 -0400

    Read anthropic key from file

--- emacsPackages/tfl-aider/tfl-aider.el
+++ emacsPackages/tfl-aider/tfl-aider.el
@@ -13,12 +13,15 @@
 (use-package aidermacs
   :bind (("C-c a" . aidermacs-transient-menu))
   :config
-  ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
-  ;; (setenv "ANTHROPIC_API_KEY" "sk-...")
-  ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
+  ;; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
+  (setenv "ANTHROPIC_API_KEY"
+          (with-temp-buffer
+            (insert-file-contents (expand-file-name "~/.anthropic"))
+            (buffer-string)))
+  ;; defun my-get-openrouter-api-key yourself elsewhere for security reasons
   ;; (setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
   :custom
-  ; See the Configuration section below
+  ;; See the Configuration section below
   (aidermacs-use-architect-mode t)
   (aidermacs-default-model "sonnet"))
 
