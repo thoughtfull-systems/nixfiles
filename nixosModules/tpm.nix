16a1922d73a655e1009663cc124f434922ecd104
technosophist <technosophist@thoughtfull.systems>
2025-09-20 13:30:15 -0400

    Install TPM2 tools when TPM is enabled

--- nixosModules/tpm.nix
+++ nixosModules/tpm.nix
@@ -2,6 +2,7 @@
   enabled = config.security.tpm2.enable;
 in lib.mkIf enabled
   {
+    environment.systemPackages = lib.mkIf enabled [ pkgs.tpm2-tools ];
     security.tpm2 = {
       pkcs11.enable = enabled;
       tctiEnvironment.enable = enabled;
