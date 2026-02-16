13999d7df45b7dd82d8eee654485407ef86fe02e
technosophist <technosophist@thoughtfull.systems>
2026-02-16 06:46:05 -0500

    Remove aider and mcp
    I'm going to use claude code instead

--- emacsPackages/default.nix
+++ emacsPackages/default.nix
@@ -1,12 +1,9 @@
 epkgs:
 (import ./tfl epkgs) //
-(import ./tfl-aider epkgs) //
 (import ./tfl-exwm epkgs) //
 (import ./tfl-gtd epkgs) //
-(import ./tfl-mcp epkgs) //
 (import ./tfl-ol-obsidian epkgs) //
 (import ./tfl-org epkgs) //
 (import ./tfl-org-bullets epkgs) //
 (import ./tfl-org-capture epkgs) //
-(import ./tfl-org-faces epkgs) //
-(import ./tfl-rust epkgs)
+(import ./tfl-org-faces epkgs)
