cae6d1270969b8b39bca74f23874effc7a0ecaf4
technosophist <technosophist@thoughtfull.systems>
2026-08-21 13:53:56 -0400

    Remove pruning from restic backup job

--- nixosModules/restic.nix
+++ nixosModules/restic.nix
@@ -75,12 +75,6 @@ in {
       exclude = cfg.exclude;
       passwordFile = pwd-path;
       paths = cfg.paths;
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 12"
-        "--keep-yearly 75"
-      ];
       repository = "s3:s3.amazonaws.com/${cfg.s3Bucket}";
       timerConfig.OnCalendar = lib.mkDefault "*-*-* *:00:00";
     };
